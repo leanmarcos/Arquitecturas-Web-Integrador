12dc0a38bfdbfa4818016c0834765e1c35244821
leanmarcos <142643919+leanmarcos@users.noreply.github.com>
2026-09-02 12:12:23 -0300

    Producto DAO + Impl

--- Integrador1/src/main/java/org/example/dao/ProductoDAO.java
+++ Integrador1/src/main/java/org/example/dao/ProductoDAO.java
@@ -1,4 +1,13 @@
 package org.example.dao;
 
+import org.example.entity.Producto;
+
+import java.util.List;
+
 public interface ProductoDAO {
+    void insert(String name, Float price);
+    void update(String name, Float price);
+    void delete(int id);
+    Producto select(int id);
+    List<Producto> getAll();
 }
---
+++ Integrador1/src/main/java/org/example/impl/ProductoDAOImpl.java
@@ -0,0 +1,107 @@
+package org.example.impl;
+
+import org.example.dao.ProductoDAO;
+import org.example.entity.Producto;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductoDAOImpl implements ProductoDAO {
+
+    private final Connection conn;
+
+    public ProductoDAOImpl(Connection conn) {
+        this.conn = conn;
+    }
+
+    @Override
+    public void insert(String name, Float price){
+        String query = "INSERT INTO producto (nombre,precio) VALUES (?,?)";
+        try(PreparedStatement stmt = conn.prepareStatement(query)){
+            stmt.setString(1, name);
+            stmt.setFloat(2, price);
+            stmt.execute();
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void update(String name, Float price){
+        String sql = "UPDATE productos SET nombre = ?, valor = ? WHERE idProducto = ?";
+        try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setString(1, name);
+            stmt.setDouble(2, price);
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void delete(int id){
+        String query = "DELETE FROM productos WHERE idProducto = ?";
+        try (PreparedStatement stmt = conn.prepareStatement(query)) {
+            stmt.setInt(1, id);
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public Producto select(int id){
+        String query = "SELECT * FROM producto WHERE idProducto = ?";
+        try(PreparedStatement stms = conn.prepareStatement(query)){
+            ResultSet rs = stms.executeQuery();
+            Producto producto = mapProduct(rs);
+
+            if(producto == null){
+                return null;
+            }
+            return producto;
+        }catch(Exception e){
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    @Override
+    public List<Producto> getAll(){
+        String query = "SELECT * FROM producto";
+        List<Producto> products = new ArrayList<>();
+
+        try(PreparedStatement stms = conn.prepareStatement(query)){
+            ResultSet rs = stms.executeQuery();
+
+            while(rs.next()){
+                Producto producto = mapProduct(rs);
+                products.add(producto);
+            }
+
+            return products;
+
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private Producto mapProduct(ResultSet rs) throws SQLException {
+        try{
+            Producto p = new Producto(rs.getInt("idProducto"),
+                    rs.getString("nombre"),
+                    rs.getFloat("valor")
+            );
+            return p;
+        }catch(Exception e){
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+}
