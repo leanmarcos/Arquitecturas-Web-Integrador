bddc557d0945c7dc17541ba485f6b3a9f08294fe
Joaquin Block <joaquin.block@gmail.com>
2026-09-02 16:19:28 -0300

    resolve Connection once in MySqlDAOFactory

--- Integrador1/src/main/java/org/example/factory/MySqlDAOFactory.java
+++ Integrador1/src/main/java/org/example/factory/MySqlDAOFactory.java
@@ -7,12 +7,18 @@ import org.example.dao.ProductoDAO;
 import org.example.connection.ConnectionManagerSingleton;
 import org.example.mysql.MySqlClienteDAO;
 
+import java.sql.Connection;
+
 public class MySqlDAOFactory extends DAOFactory {
 
+    private Connection conn;
 
+    public MySqlDAOFactory() {
+        this.conn = ConnectionManagerSingleton.getInstance().getConnection();
+    }
     @Override
     public ClienteDAO getClienteDAO() {
-        return new MySqlClienteDAO(ConnectionManagerSingleton.getInstance().getConnection());
+        return new MySqlClienteDAO(conn);
     }
 
     @Override
