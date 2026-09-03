1a320a01ed7c59323c75658d758224490f85d457
Joaquin Block <joaquin.block@gmail.com>
2026-09-03 11:28:58 -0300

    refactor: inject Connection into DAOFactory instead of resolving it from the Singleton
    - Add DbEngine enum to replace the magic int used to pick the DAO implementation
    - DAOFactory.getInstance now takes a DbEngine and a Connection instead of building one internally
    - MySqlDAOFactory receives the Connection via constructor instead of calling ConnectionManagerSingleton internally

--- Integrador1/src/main/java/org/example/factory/DAOFactory.java
+++ Integrador1/src/main/java/org/example/factory/DAOFactory.java
@@ -5,27 +5,21 @@ import org.example.dao.FacturaDAO;
 import org.example.dao.FacturaProductoDAO;
 import org.example.dao.ProductoDAO;
 
+import java.sql.Connection;
+
 public abstract class DAOFactory{
 
-    private static final int mysql = 1;
-    private static DAOFactory instance;
-
-    public static DAOFactory getInstance(int db){
-       if (instance==null){
+    public static DAOFactory getInstance(DbEngine db, Connection connection) {
            switch (db){
-               case mysql:
-                   instance = new MySqlDAOFactory();
-                   break;
+               case MYSQL:
+                   return new MySqlDAOFactory(connection);
                default:
-                   return null;
+                   throw  new IllegalArgumentException("Motor no soportado: " + db);
            }
-       }
-       return instance;
     }
 
     public abstract ClienteDAO getClienteDAO();
     public abstract FacturaDAO getFacturaDAO();
     public abstract ProductoDAO getProductoDAO();
     public abstract FacturaProductoDAO getFacturaProductoDAO();
-
 }
---
+++ Integrador1/src/main/java/org/example/factory/DbEngine.java
@@ -0,0 +1,5 @@
+package org.example.factory;
+
+public enum DbEngine {
+    MYSQL
+}
