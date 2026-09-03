ec46fd397c4ef64bd22c57706c5ba6590c35f3d3
leanmarcos <142643919+leanmarcos@users.noreply.github.com>
2026-09-03 14:59:17 -0300

    fix borrado de función tras conflicto

--- Integrador1/src/main/java/org/example/mysql/MySqlFacturaDAO.java
+++ Integrador1/src/main/java/org/example/mysql/MySqlFacturaDAO.java
@@ -4,6 +4,7 @@ import org.example.dao.FacturaDAO;
 import org.example.entity.Factura;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.util.List;
 
 public class MySqlFacturaDAO implements FacturaDAO {
@@ -13,7 +14,7 @@ public class MySqlFacturaDAO implements FacturaDAO {
         this.connection = connection;
     }
 
-
+    @Override
     public void insertFactura(List<Factura> facturas) {
         // Implementación de la inserción de facturas en la base de datos MySQL
         String sql = "INSERT INTO factura(idCliente, idFactura) VALUES (?, ?)";
@@ -27,6 +28,17 @@ public class MySqlFacturaDAO implements FacturaDAO {
             System.out.println("Insertadas " + facturas.size() + " facturas.");
         } catch (Exception e) {
             System.err.println("Error al insertar facturas: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void deleteFacturaById(int id){
+        String query = "DELETE FROM  factura WHERE idFactura = ?";
+        try(PreparedStatement ps = connection.prepareStatement(query)){
+            ps.setInt(1, id);
+            ps.executeUpdate();
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
