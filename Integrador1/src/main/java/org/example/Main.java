35ee7ffc00625efff19e8abaf78a7d1355f207e8
Manuel <manumontorp@gmail.com>
2026-09-04 12:58:23 -0300

    fix: main.java
    load data from DataLoader

--- Integrador1/src/main/java/org/example/Main.java
+++ Integrador1/src/main/java/org/example/Main.java
@@ -1,13 +1,13 @@
 package org.example;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
 import org.example.connection.ConnectionManagerSingleton;
 import org.example.csv.CsvImporter;
 import org.example.factory.DAOFactory;
 import org.example.factory.DbEngine;
 import org.example.mysql.DataLoader;
-
-import java.sql.Connection;
-import java.sql.SQLException;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -20,5 +20,6 @@ public class Main {
         factory.getSchemaDAO().createTables();
 
         DataLoader loader = new DataLoader(conn, factory, csvImporter);
+        loader.loadAllData();
     }
 }
