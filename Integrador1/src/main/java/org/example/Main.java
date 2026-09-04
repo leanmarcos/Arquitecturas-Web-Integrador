61766bc135bcf1f8f86b92171faab88db6879f95
Joaquin Block <joaquin.block@gmail.com>
2026-09-03 23:35:11 -0300

    feat: create database schema on startup
    - Expose SchemaDAO through DAOFactory/MySqlDAOFactory
    - Call createTables() from Main so schema creation is verified end to end

--- Integrador1/src/main/java/org/example/Main.java
+++ Integrador1/src/main/java/org/example/Main.java
@@ -7,14 +7,17 @@ import org.example.factory.DbEngine;
 import org.example.mysql.DataLoader;
 
 import java.sql.Connection;
+import java.sql.SQLException;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         Connection  conn = ConnectionManagerSingleton.getInstance().getConnection();
         CsvImporter csvImporter = new CsvImporter();
         DAOFactory factory = DAOFactory.getInstance(DbEngine.MYSQL, conn);
+
+        factory.getSchemaDAO().createTables();
 
         DataLoader loader = new DataLoader(conn, factory, csvImporter);
     }
