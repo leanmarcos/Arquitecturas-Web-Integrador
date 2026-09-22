993c6cb3fd6f581ab47f7a972629fc37483a56b2
leanmarcos <142643919+leanmarcos@users.noreply.github.com>
2026-09-22 13:01:31 -0300

    Getters en carrera

--- Integrador2/src/main/java/org/example/model/Carrera.java
+++ Integrador2/src/main/java/org/example/model/Carrera.java
@@ -1,10 +1,13 @@
 package org.example.model;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+
 import java.util.List;
 
 @Entity
 @Table(name = "carrera")
+@Getter
 public class Carrera {
 
     @Id
