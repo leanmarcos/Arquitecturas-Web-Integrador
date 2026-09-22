84a362953937017441c9b758981bb96580dd1941
Joaquin Block <joaquin.block@gmail.com>
2026-09-21 22:53:30 -0300

    feat: verify JPA connection on startup

--- Integrador2/src/main/java/org/example/Main.java
+++ Integrador2/src/main/java/org/example/Main.java
@@ -1,7 +1,14 @@
 package org.example;
 
+import jakarta.persistence.EntityManager;
+import org.example.utils.JPAUtil;
+
 public class Main {
     public static void main(String[] args) {
-
+        try (EntityManager em = JPAUtil.getEntityManager()) {
+            System.out.println("Conexión OK: " + em.isOpen());
+        } finally {
+            JPAUtil.close();
+        }
     }
 }
