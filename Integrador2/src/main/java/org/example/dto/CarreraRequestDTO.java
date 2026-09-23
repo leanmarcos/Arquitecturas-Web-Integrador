d36f49fcdf2406d87cad1d5d0381fd1ad0c9a986
leanmarcos <142643919+leanmarcos@users.noreply.github.com>
2026-09-23 14:53:44 -0300

    DTOs modificados

--- Integrador2/src/main/java/org/example/dto/CarreraRequestDTO.java
+++ Integrador2/src/main/java/org/example/dto/CarreraRequestDTO.java
@@ -1,4 +1,17 @@
 package org.example.dto;
 
+import lombok.Data;
+import lombok.Getter;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Data
 public class CarreraRequestDTO {
+
+    @Getter
+    @NotBlank(message = "El nombre de la carrera es obligatorio")
+    @Size(max = 255, message = "El nombre no debe superar los 255 caracteres")
+    private String nombre;
+
 }
--- Integrador2/src/main/java/org/example/dto/ErrorResponseDTO.java
+++ Integrador2/src/main/java/org/example/dto/ErrorResponseDTO.java
@@ -1,4 +1,23 @@
 package org.example.dto;
 
+import lombok.Data;
+import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Data
 public class ErrorResponseDTO {
+
+    @NotNull
+    @Setter
+    private Integer errorCode;
+
+    @NotBlank
+    @Setter
+    private String errorMessage;
+
+    @Setter
+    private String details;
+
 }
