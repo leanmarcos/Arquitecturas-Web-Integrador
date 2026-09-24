a9058bc5e756a3e39225d98bc8de441a47214ceb
micadiazz1 <micaelaexactas1@gmail.com>
2026-09-24 16:24:57 -0300

    feat: issue #44 consulta estudiante de una carrera por cuidad

--- Integrador2/src/main/java/org/example/service/EstudianteCarreraService.java
+++ Integrador2/src/main/java/org/example/service/EstudianteCarreraService.java
@@ -1,4 +1,34 @@
 package org.example.service;
 
+import jakarta.persistence.EntityManager;
+import org.example.dto.EstudianteResponseDTO;
+import org.example.model.Carrera;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.example.dto.EstudianteResponseDTO;
+import org.example.mapper.EstudianteMapper;
+import org.example.model.Estudiante;
+import org.example.repository.EstudianteRepositoryImpl;
+import org.example.utils.JPAUtil;
+
 public class EstudianteCarreraService {
+
+    private EstudianteRepositoryImpl studentRepository;
+
+    //Recuperar estudiantes de una carrera filtrados por ciudad
+    public List<EstudianteResponseDTO> getCareerStudentsByCity(Carrera carrera, String city){
+        if (carrera == null || carrera.getId() == null) throw new IllegalArgumentException("La carrera es obligatoria.");
+        if (city == null || city.isBlank()) throw new IllegalArgumentException("La ciudad es obligatoria.");
+
+        try (EntityManager em = JPAUtil.getEntityManager()) {
+            List<Estudiante> students = studentRepository.findStudentsByCarreraAndCity(em, carrera, city.trim());
+
+            List<EstudianteResponseDTO> responseDTOs = new ArrayList<>();
+            for (Estudiante student : students) {
+                responseDTOs.add(EstudianteMapper.toDto(student));
+            }
+            return responseDTOs;
+        }
+    }
 }
