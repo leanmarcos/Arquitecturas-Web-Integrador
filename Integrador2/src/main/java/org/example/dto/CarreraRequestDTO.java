8e058003a8ce411946f32db2b866264f669d35e8
leanmarcos <142643919+leanmarcos@users.noreply.github.com>
2026-09-25 10:20:26 -0300

    DTOs convertidos a Record

--- Integrador2/src/main/java/org/example/dto/CarreraRequestDTO.java
+++ Integrador2/src/main/java/org/example/dto/CarreraRequestDTO.java
@@ -2,16 +2,17 @@ package org.example.dto;
 
 import lombok.Data;
 import lombok.Getter;
+import lombok.Setter;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
-@Data
-public class CarreraRequestDTO {
 
-    @Getter
+public record CarreraRequestDTO (
     @NotBlank(message = "El nombre de la carrera es obligatorio")
     @Size(max = 255, message = "El nombre no debe superar los 255 caracteres")
-    private String nombre;
+    String nombre
+
+){
 
 }
